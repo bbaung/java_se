85951b232499bb3150291a4db28d531f880779c1
arjun <arjunvisibleone.arjun@gmail.com>
2025-04-27 12:44:44 +0630

    fix: java-syntax

--- 02-java-syntax/Hello.java
+++ 02-java-syntax/Hello.java
@@ -1,1 +1,14 @@
-public
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    //run the program with: java Hello
+    //run the program with: java Hello hi nice to meet you
+    //run the program with: java Hello "hi nice to meet you"
+
+        for (String s : args) {
+            System.out.println(s);
+        }
+
+    }
+}
