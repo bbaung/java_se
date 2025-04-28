442ece1f0a7f6e26f41059ec961bbba1d057236c
bbaung <bbaung4109@gmail.com>
2025-04-28 07:55:25 +0630

    fix: LocalVariableTest

--- 04-variable-type/LocalVariableTest.java
+++ 04-variable-type/LocalVariableTest.java
@@ -1,1 +1,44 @@
-hello
+public class LocalVariableTest{
+
+    
+    static String message = "Global variable message";
+
+    public static void main(String[] args) {
+        System.out.println(message);
+
+        doSomething();
+
+        doSomethingGlobal();
+
+        LocalVariableTest obj1 = new LocalVariableTest(); //instance variable / object variable can only be accessed through an object
+        obj1.hello();
+        obj1.helloGlobal();
+    }
+
+    public static void doSomething(){
+        String message = "Do Something"; //variable hiding
+
+        System.out.println(message);
+    }
+
+    public static void doSomethingGlobal(){
+        String message = "Local variable";
+
+        System.out.println(LocalVariableTest.message);
+    }
+
+    public void hello(){
+        String message = "hello";
+
+        System.out.println(message);
+    }
+
+    public void helloGlobal(){
+        String message = "Local variable hello";
+
+        System.out.println(this.message); //most prefer
+
+        System.out.println(LocalVariableTest.message);
+    }
+}
+
