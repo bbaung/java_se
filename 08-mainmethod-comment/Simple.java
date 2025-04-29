54508682740cd75ba7afb2bf3068ded3d82123fe
bbaung <bbaung4109@gmail.com>
2025-04-30 04:02:40 +0630

    add: mainmethod-comment

---
+++ 08-mainmethod-comment/Simple.java
@@ -0,0 +1,13 @@
+public class Simple{
+
+    public static void main(String[] args) {
+        System.out.println("This is main method");
+
+        int x;
+        x = 20;
+        // x = 30;
+        x = 40;
+
+        System.out.println(x);
+    }
+}
