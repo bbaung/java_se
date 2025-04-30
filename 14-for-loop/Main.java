cfec9dacf5b9e3703c4146ca4a00a95a7ea2b337
arjun <arjunvisibleone.arjun@gmail.com>
2025-05-01 05:48:10 +0630

    add: for-loop

---
+++ 14-for-loop/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");   
+        }
+
+        /*
+         * Explain for loop
+         * for (initialExpression;testExpression;updateExpression){
+         * //body of the loop}
+         */
+    }
+}
