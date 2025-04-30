b157c6b4304f7a8d69086a63ac6f9330e257ff70
arjun <arjunvisibleone.arjun@gmail.com>
2025-04-30 18:40:27 +0630

    add: java-variable

---
+++ 09-java-variable/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //Initalization
+        int x = 20;
+
+
+        //declaration
+        int y;
+
+        //assing value
+        y = 50;
+
+        System.out.println(y);
+
+
+        //int z = 100;
+        //variable_type variable_name = variable_value;
+    }
+}
