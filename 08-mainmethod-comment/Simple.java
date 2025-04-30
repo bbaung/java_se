bf9c043237a67a4fac49cb386be21d77a290ed92
arjun <arjunvisibleone.arjun@gmail.com>
2025-04-30 18:02:12 +0630

    add: mainmethod-comment

--- 08-mainmethod-comment/Simple.java
+++ 08-mainmethod-comment/Simple.java
@@ -5,9 +5,18 @@ public class Simple{
 
         int x;
         x = 20;
+        //single line-comment
         // x = 30;
         x = 40;
 
         System.out.println(x);
     }
+
+
+    /* Example of multi-line comment */
+
+    /* 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    } */
 }
